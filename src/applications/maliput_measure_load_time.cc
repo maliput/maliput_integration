074474f5c83c33e01e5fbaa789e0aed39a4a9b4f
Franco Cipollone <franco.c@ekumenlabs.com>
2021-02-19 16:09:48 -0300

    Adds app for measuring load time process.

---
+++ src/applications/maliput_measure_load_time.cc
@@ -0,0 +1,102 @@
+// Copyright 2021 Toyota Research Institute
+/// @file maliput_measure_load_time.cc
+///
+/// Builds an api::RoadGeometry as many times as requested and calculates a mean with the time results. Possible
+/// backends are `dragway`, `multilane` and `malidrive`.
+///
+/// @note
+///   1. Allows to load a road geometry from different road geometry implementations.
+///       The `maliput_backend` flag will determine the backend to be used.
+///      - "dragway": The following flags are supported to use in order to create dragway road geometry:
+///           -num_lanes, -length, -lane_width, -shoulder_width, -maximum_height.
+///      - "multilane": yaml file path must be provided:
+///           -yaml_file.
+///      - "malidrive": xodr file path must be provided and other arguments are optional:
+///           -xodr_file_path -linear_tolerance -build_policy -num_threads.
+///   2. The applications allows you to load a xodr multiple times and calculate a mean.
+///      The number of iterations could be changed using:
+///      -iterations
+///   3. The level of the logger is selected with `-log_level`.
+
+#include <chrono>
+#include <numeric>
+#include <string>
+#include <vector>
+
+#include <gflags/gflags.h>
+
+#include "integration/tools.h"
+#include "maliput_gflags.h"
+
+#include "maliput/common/logger.h"
+
+namespace maliput {
+namespace integration {
+namespace {
+
+MULTILANE_PROPERTIES_FLAGS();
+DRAGWAY_PROPERTIES_FLAGS();
+MALIDRIVE_PROPERTIES_FLAGS();
+MALIPUT_APPLICATION_DEFINE_LOG_LEVEL_FLAG();
+
+DEFINE_string(maliput_backend, "malidrive",
+              "Whether to use <dragway>, <multilane> or <malidrive>. Default is malidrive.");
+DEFINE_int32(iterations, 1, "Number of iterations for loading the Road Geometry.");
+
+// Measure the time that it takes to create the RoadNetwork using the implementation that `maliput_implementation`
+// describes. It is a wrapper around maliput::integration::LoadRoadNetwork() method.
+//
+// @param maliput_implementation One of MaliputImplementation. (kDragway, kMultilane, kMalidrive).
+// @param dragway_build_properties Holds the properties to build a dragway RoadNetwork.
+// @param multilane_build_properties Holds the properties to build a multilane RoadNetwork.
+// @param malidrive_build_properties Holds the properties to build a malidrive RoadNetwork.
+// @return the time in seconds.
+//
+// @throw maliput::common::assertion_error When `maliput_implementation` is unknown.
+double MeasureLoadTime(MaliputImplementation maliput_implementation,
+                       const DragwayBuildProperties& dragway_build_properties,
+                       const MultilaneBuildProperties& multilane_build_properties,
+                       const MalidriveBuildProperties& malidrive_build_properties) {
+  auto start = std::chrono::high_resolution_clock::now();
+  auto rn = LoadRoadNetwork(maliput_implementation, dragway_build_properties, multilane_build_properties,
+                            malidrive_build_properties);
+  auto end = std::chrono::high_resolution_clock::now();
+  std::chrono::duration<double> duration = (end - start);
+  return duration.count();
+}
+
+int Main(int argc, char* argv[]) {
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+  maliput::common::set_log_level(FLAGS_log_level);
+
+  log()->debug("Backend implementation selected is {} ", FLAGS_maliput_backend);
+  const MaliputImplementation maliput_implementation{StringToMaliputImplementation(FLAGS_maliput_backend)};
+
+  std::vector<double> times;
+  if (FLAGS_iterations < 1) {
+    log()->error("Iterations: {}. The number of iterations must be greater than zero. Using iterations=1.",
+                 FLAGS_iterations);
+    FLAGS_iterations = 1;
+  }
+  times.reserve(FLAGS_iterations);
+  const std::optional<int> num_threads{FLAGS_num_threads > 0 ? std::make_optional<int>(FLAGS_num_threads)
+                                                             : std::nullopt};
+  for (int i = 0; i < FLAGS_iterations; i++) {
+    times.push_back(MeasureLoadTime(
+        maliput_implementation,
+        {FLAGS_num_lanes, FLAGS_length, FLAGS_lane_width, FLAGS_shoulder_width, FLAGS_maximum_height},
+        {FLAGS_yaml_file},
+        {FLAGS_xodr_file_path, FLAGS_linear_tolerance, FLAGS_build_policy, FLAGS_num_threads, FLAGS_road_rule_book_file,
+         FLAGS_traffic_light_book_file, FLAGS_phase_ring_book_file, FLAGS_intersection_book_file}));
+  }
+  const double mean_time = (std::accumulate(times.begin(), times.end(), 0.)) / static_cast<double>(times.size());
+  maliput::log()->info("\tMean time after {} iterations is ----> {} seconds.\n", FLAGS_iterations, mean_time);
+
+  return 0;
+}
+
+}  // namespace
+}  // namespace integration
+}  // namespace maliput
+
+int main(int argc, char* argv[]) { return maliput::integration::Main(argc, argv); }
