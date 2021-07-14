922202c33b2c7fbfc6a94096e5dd37edafae6492
Franco Cipollone <franco.c@ekumenlabs.com>
2021-07-14 09:18:59 -0300

    Print road network loading status for maliput_to_string app.

--- src/applications/maliput_to_string.cc
+++ src/applications/maliput_to_string.cc
@@ -52,7 +52,7 @@ int Main(int argc, char* argv[]) {
   gflags::ParseCommandLineFlags(&argc, &argv, true);
   maliput::common::set_log_level(FLAGS_log_level);
 
-  log()->debug("Loading road network using {} backend implementation...", FLAGS_maliput_backend);
+  log()->info("Loading road network using {} backend implementation...", FLAGS_maliput_backend);
   const MaliputImplementation maliput_implementation{StringToMaliputImplementation(FLAGS_maliput_backend)};
   auto rn = LoadRoadNetwork(
       maliput_implementation,
@@ -61,7 +61,7 @@ int Main(int argc, char* argv[]) {
        FLAGS_tolerance_selection_policy, FLAGS_standard_strictness_policy, FLAGS_omit_nondrivable_lanes,
        FLAGS_road_rule_book_file, FLAGS_traffic_light_book_file, FLAGS_phase_ring_book_file,
        FLAGS_intersection_book_file});
-  log()->debug("RoadNetwork loaded successfully.");
+  log()->info("RoadNetwork loaded successfully.");
 
   const maliput::utility::GenerateStringOptions options{FLAGS_include_type_labels,  FLAGS_include_road_geometry_id,
                                                         FLAGS_include_junction_ids, FLAGS_include_segment_ids,
