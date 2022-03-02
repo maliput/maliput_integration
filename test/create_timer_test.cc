6a9c59b39be64dc1853cb48168101ea3840ace64
Franco Cipollone <franco.c@ekumenlabs.com>
2022-03-02 12:25:34 -0300

    Adds tests for timers.

---
+++ test/create_timer_test.cc
@@ -0,0 +1,22 @@
+#include "integration/create_timer.h"
+
+#include <chrono>
+#include <memory>
+
+#include <gtest/gtest.h>
+
+#include "integration/chrono_timer.h"
+
+namespace maliput {
+namespace integration {
+namespace {
+
+GTEST_TEST(CreateTimerTest, CreateTimer) {
+  std::unique_ptr<Timer> timer = CreateTimer(TimerType::kChronoTimer);
+  EXPECT_NE(timer, nullptr);
+  EXPECT_NE(dynamic_cast<ChronoTimer*>(timer.get()), nullptr);
+}
+
+}  // namespace
+}  // namespace integration
+}  // namespace maliput
