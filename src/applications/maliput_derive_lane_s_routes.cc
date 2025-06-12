5d09cbbbad2bf94cb2d79f94869a39ed2b74427e
Franco Cipollone <53065142+francocipollone@users.noreply.github.com>
2025-06-12 11:28:58 -0300

    Adds malidrive param: Integrator accuracy multiplier (#144)
    Signed-off-by: Franco Cipollone <franco.c@ekumenlabs.com>

--- src/applications/maliput_derive_lane_s_routes.cc
+++ src/applications/maliput_derive_lane_s_routes.cc
@@ -320,8 +320,8 @@ int main(int argc, char* argv[]) {
       {FLAGS_num_lanes, FLAGS_length, FLAGS_lane_width, FLAGS_shoulder_width, FLAGS_maximum_height}, {yaml_file},
       {xodr_file, GetLinearToleranceFlag(), GetMaxLinearToleranceFlag(), GetAngularToleranceFlag(), FLAGS_build_policy,
        FLAGS_num_threads, FLAGS_simplification_policy, FLAGS_standard_strictness_policy, FLAGS_omit_nondrivable_lanes,
-       FLAGS_rule_registry_file, FLAGS_road_rule_book_file, FLAGS_traffic_light_book_file, FLAGS_phase_ring_book_file,
-       FLAGS_intersection_book_file},
+       FLAGS_integrator_accuracy_multiplier, FLAGS_rule_registry_file, FLAGS_road_rule_book_file,
+       FLAGS_traffic_light_book_file, FLAGS_phase_ring_book_file, FLAGS_intersection_book_file},
       {FLAGS_osm_file, FLAGS_linear_tolerance, FLAGS_angular_tolerance, maliput::math::Vector2::FromStr(FLAGS_origin),
        FLAGS_rule_registry_file, FLAGS_road_rule_book_file, FLAGS_traffic_light_book_file, FLAGS_phase_ring_book_file,
        FLAGS_intersection_book_file});
